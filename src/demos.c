c8919845c63fa503b3b27b9a46752fe3c82e44e5
indigoparadox <indigo.repo@interfinitydynamics.info>
2023-01-13 18:17:24 -0500

    More work on parser.

--- src/demos.c
+++ src/demos.c
@@ -7,7 +7,7 @@
 #define RETROGLU_C
 #include "demos.h"
 
-void draw_cube_iter( void* data ) {
+void draw_cube_iter( struct DEMO_DATA* data ) {
    struct RETROFLAT_INPUT input_evt;
    long unsigned int next = 0;
    int input = 0;
@@ -16,6 +16,7 @@ void draw_cube_iter( void* data ) {
 
    /* Input */
 
+   input_evt.allow_repeat = 1;
    input = retroflat_poll_input( &input_evt );
 
    switch( input ) {
@@ -138,86 +139,138 @@ void draw_cube_iter( void* data ) {
    next = retroflat_get_ms() + retroflat_fps_next();
 }
 
-void draw_tree_iter( void* data ) {
+int demo_load_obj(
+   const char* filename, struct RETROGLU_PARSER* parser, struct DEMO_DATA* data
+) {
+   FILE* obj_file = NULL;
+   unsigned long int i = 0; /* Index in file buffer, so long. */
+   long int obj_read = 0;
+   int auto_parser = 0; /* Did we provision parser? */
+   unsigned char* obj_buf = NULL;
+   unsigned long int obj_buf_sz = 0;
+
+   if( NULL == parser ) {
+      parser = calloc( 1, sizeof( struct RETROGLU_PARSER ) );
+      assert( NULL != parser );
+      auto_parser = 1;
+   }
+
+   /* Open the file and allocate the buffer. */
+   obj_file = fopen( filename, "r" );
+   assert( NULL != obj_file );
+   fseek( obj_file, 0, SEEK_END );
+   obj_buf_sz = ftell( obj_file );
+   fseek( obj_file, 0, SEEK_SET );
+   printf( "opened %s, %lu bytes\n", filename, obj_buf_sz );
+   obj_buf = calloc( 1, obj_buf_sz );
+   assert( NULL != obj_buf );
+   obj_read = fread( obj_buf, 1, obj_buf_sz, obj_file );
+   assert( obj_read == obj_buf_sz );
+   fclose( obj_file );
+
+   retroglu_parse_init(
+      parser,
+      data->vertices, data->vertices_sz, DEMO_VERTICES_SZ_MAX,
+      data->vnormals, data->vnormals_sz, DEMO_VERTICES_SZ_MAX,
+      data->vtextures, data->vtextures_sz, DEMO_VERTICES_SZ_MAX,
+      data->faces, data->faces_sz, DEMO_FACES_SZ_MAX,
+      data->materials, data->materials_sz, DEMO_MATERIALS_SZ_MAX,
+      (retroglu_mtl_cb)demo_load_obj, (void*)data );
+
+   /* Parse the obj, byte by byte. */
+   for( i = 0 ; obj_buf_sz > i ; i++ ) {
+      obj_read = retroglu_parse_obj_c( parser, obj_buf[i] );
+      assert( 0 <= obj_read );
+   }
+   free( obj_buf );
+   obj_buf = NULL;
+   obj_buf_sz = 0;
+   data->vertices_sz = parser->vertices_sz;
+   data->vnormals_sz = parser->vnormals_sz;
+   data->vtextures_sz = parser->vtextures_sz;
+   data->faces_sz = parser->faces_sz;
+   data->materials_sz = parser->materials_sz;
+
+   if( auto_parser ) {
+      free( parser );
+      parser = NULL;
+   }
+
+   return RETROFLAT_OK;
+}
+
+void demo_dump_obj( const char* filename, struct DEMO_DATA* data ) {
+   FILE* obj_file = NULL;
+   unsigned long int i = 0, /* Index in file buffer, so long. */
+      j = 0;
+
+   /* Dump */
+   obj_file = fopen( "test.txt", "w" );
+   assert( NULL != obj_file );
+   for( i = 0 ; data->vertices_sz > i ; i++ ) {
+      fprintf( obj_file, "v %f %f %f\n",
+         data->vertices[i].x, data->vertices[i].y, data->vertices[i].z );
+   }
+
+   fprintf( obj_file, "\n" );
+   
+   for( i = 0 ; data->vnormals_sz > i ; i++ ) {
+      fprintf( obj_file, "vn %f %f %f\n",
+         data->vnormals[i].x, data->vnormals[i].y, data->vnormals[i].z );
+   }
+
+   for( i = 0 ; data->faces_sz > i ; i++ ) {
+      if(
+         0 == i ||
+         data->faces[i].material_idx != data->faces[i - 1].material_idx
+      ) {
+         fprintf( obj_file, "\nusemtl %s\n\n",
+            data->materials[data->faces[i].material_idx].name );
+      }
+      fprintf( obj_file, "f " );
+      for( j = 0 ; data->faces[i].vertex_idxs_sz > j ; j++ ) {
+         fprintf( obj_file, "%d/%d/%d",
+            data->faces[i].vertex_idxs[j],
+            data->faces[i].vtexture_idxs[j],
+            data->faces[i].vnormal_idxs[j] );
+         if( j + 1 < data->faces[i].vertex_idxs_sz ) {
+            fprintf( obj_file, " " );
+         }
+      }
+      fprintf( obj_file, "\n" );
+   }
+
+   fclose( obj_file );
+   obj_file = NULL;
+}
+
+void draw_tree_iter( struct DEMO_DATA* data ) {
    struct RETROFLAT_INPUT input_evt;
    long unsigned int next = 0;
    int input = 0;
+   int i = 0,
+      j = 0;
    static int rotate_x = 10;
    static int rotate_y = 10;
-   FILE* tree_file = NULL;
-   static struct RETROGLU_VERTEX vertices[DEMO_VERTICES_SZ_MAX];
-   static int vertices_sz = 0;
-   static struct RETROGLU_FACE faces[DEMO_FACES_SZ_MAX];
-   static int faces_sz = 0;
-   struct RETROGLU_PARSER parser;
-   long int tree_read = 0;
-   unsigned long int i = 0; /* Also index in file buffer, so long. */
-   unsigned int j = 0;
    static float tx = 0.0f,
       ty = 0.0f,
       tz = 0.0f;
-   float mcolor = 0.0f;
+   const float l_diffuse[] = {1.0f, 1.0f, 1.0f, 1.0f};
+   const float l_position[] = {1.0f, 1.0f, 0.3f, 0.0f};
 
-   if( 0 == vertices_sz ) {
-      tree_file = fopen( "treeHigh.obj", "r" );
-      assert( NULL != tree_file );
-      fseek( tree_file, 0, SEEK_END );
-      parser.obj_buf_sz = ftell( tree_file );
-      fseek( tree_file, 0, SEEK_SET );
-      printf( "opened treeHigh.obj, %lu bytes\n", parser.obj_buf_sz );
-      parser.obj_buf = calloc( 1, parser.obj_buf_sz );
-      assert( NULL != parser.obj_buf );
-      tree_read = fread( parser.obj_buf, 1, parser.obj_buf_sz, tree_file );
-      assert( tree_read == parser.obj_buf_sz );
-      fclose( tree_file );
+   if( 0 == data->vertices_sz ) {
+      demo_load_obj( "treeHigh.obj", NULL, data );
 
-      retroglu_parse_init(
-         &parser,
-         vertices, DEMO_VERTICES_SZ_MAX, faces, DEMO_FACES_SZ_MAX );
-      for( i = 0 ; parser.obj_buf_sz > i ; i++ ) {
-         tree_read = retroglu_parse_obj_c( &parser, parser.obj_buf[i] );
-         assert( 0 <= tree_read );
-      }
-      free( parser.obj_buf );
-      parser.obj_buf = NULL;
-      parser.obj_buf_sz = 0;
-      vertices_sz = parser.vertices_sz;
-      faces_sz = parser.faces_sz;
-      assert( 0 < vertices_sz );
-      assert( 0 < faces_sz );
+      assert( 0 < data->vertices_sz );
+      assert( 0 < data->faces_sz );
+      assert( 0 < data->materials_sz );
 
-      /* Dump */
-      tree_file = fopen( "test.txt", "w" );
-      assert( NULL != tree_file );
-      for( i = 0 ; vertices_sz > i ; i++ ) {
-         fprintf( tree_file, "v %f %f %f\n",
-            vertices[i].x, vertices[i].y, vertices[i].z );
-      }
-      for( i = 0 ; faces_sz > i ; i++ ) {
-         if(
-            0 == i ||
-            0 != strncmp(
-               faces[i].material, faces[i - 1].material,
-               RETROGLU_FACE_MATERIAL_SZ_MAX )
-         ) {
-            fprintf( tree_file, "\nusemtl %s\n\n", faces[i].material );
-         }
-         fprintf( tree_file, "f " );
-         for( j = 0 ; faces[i].vertice_idxs_sz > j ; j++ ) {
-            fprintf( tree_file, "%d", faces[i].vertice_idxs[j] );
-            if( j + 1 < faces[i].vertice_idxs_sz ) {
-               fprintf( tree_file, "/" );
-            }
-         }
-         fprintf( tree_file, "\n" );
-      }
-      fclose( tree_file );
-      tree_file = NULL;
-
+      demo_dump_obj( "test.txt", data );
    }
 
    /* Input */
 
+   input_evt.allow_repeat = 1;
    input = retroflat_poll_input( &input_evt );
 
    switch( input ) {
@@ -272,7 +325,10 @@ void draw_tree_iter( void* data ) {
    glRotatef( rotate_x, 1.0f, 0.0f, 0.0f );
    glRotatef( rotate_y, 0.0f, 1.0f, 0.0f );
 
-   for( i = 0 ; faces_sz > i ; i++ ) {
+   glLightfv( GL_LIGHT0, GL_POSITION, l_position );
+   glLightfv( GL_LIGHT0, GL_DIFFUSE, l_diffuse );
+
+   for( i = 0 ; data->faces_sz > i ; i++ ) {
       glBegin( GL_TRIANGLES );
    
       /*
@@ -289,18 +345,25 @@ void draw_tree_iter( void* data ) {
       }
       */
 
-      glColor3f(
-         hash_mat_r( faces[i].material ),
-         hash_mat_g( faces[i].material ),
-         hash_mat_b( faces[i].material ) );
+      glColor3fv(
+         data->materials[data->faces[i].material_idx].diffuse );
+
+      glMaterialfv( GL_FRONT, GL_DIFFUSE,
+         data->materials[data->faces[i].material_idx].diffuse );
    
-      for( j = 0 ; faces[i].vertice_idxs_sz > j ; j++ ) {
-         assert( 0 < faces[i].vertice_idxs[j] );
-         assert( 3 == faces[i].vertice_idxs_sz );
+      for( j = 0 ; data->faces[i].vertex_idxs_sz > j ; j++ ) {
+         assert( 0 < data->faces[i].vertex_idxs[j] );
+         assert( 3 == data->faces[i].vertex_idxs_sz );
+
+         glNormal3f(
+            data->vnormals[data->faces[i].vnormal_idxs[j] - 1].x,
+            data->vnormals[data->faces[i].vnormal_idxs[j] - 1].y,
+            data->vnormals[data->faces[i].vnormal_idxs[j] - 1].z );
+
          glVertex3f(
-            vertices[faces[i].vertice_idxs[j] - 1].x,
-            vertices[faces[i].vertice_idxs[j] - 1].y,
-            vertices[faces[i].vertice_idxs[j] - 1].z );
+            data->vertices[data->faces[i].vertex_idxs[j] - 1].x,
+            data->vertices[data->faces[i].vertex_idxs[j] - 1].y,
+            data->vertices[data->faces[i].vertex_idxs[j] - 1].z );
       }
 
       glEnd();
--- src/demos.h
+++ src/demos.h
@@ -7,14 +7,29 @@
 
 #define DEMO_VERTICES_SZ_MAX 1024
 #define DEMO_FACES_SZ_MAX 1024
+#define DEMO_MATERIALS_SZ_MAX 1024
+
 #define DEMO_ZOOM_INC 0.1f
 
 #define hash_mat_r( m ) (fmod( m[0] * 0.01f, 1.0f ) * 2)
 #define hash_mat_g( m ) (fmod( m[1] * 0.01f, 1.0f ) * 2)
 #define hash_mat_b( m ) (fmod( m[2] * 0.01f, 1.0f ) * 2)
 
-void draw_cube_iter( void* data );
-void draw_tree_iter( void* data );
+struct DEMO_DATA {
+   struct RETROGLU_VERTEX vertices[DEMO_VERTICES_SZ_MAX];
+   int vertices_sz;
+   struct RETROGLU_VERTEX vnormals[DEMO_VERTICES_SZ_MAX];
+   int vnormals_sz;
+   struct RETROGLU_VERTEX vtextures[DEMO_VERTICES_SZ_MAX];
+   int vtextures_sz;
+   struct RETROGLU_FACE faces[DEMO_FACES_SZ_MAX];
+   int faces_sz;
+   struct RETROGLU_MATERIAL materials[DEMO_MATERIALS_SZ_MAX];
+   int materials_sz;
+};
+
+void draw_cube_iter( struct DEMO_DATA* data );
+void draw_tree_iter( struct DEMO_DATA* data );
 
 #ifdef DEMOS_C
 
@@ -24,7 +39,7 @@ const char* gc_demo_names[] = {
 };
 
 retroflat_loop_iter gc_demo_loops[] = {
-   draw_tree_iter,
+   (retroflat_loop_iter)draw_tree_iter,
    NULL
 };
 
